c8bc8f7400f6f1380a1556ce423f889f95159d0f
chaggysen <sen.wang@mail.mcgill.ca>
2022-03-14 13:24:11 -0400

    hw4

--- hw4.ml
+++ hw4.ml
@@ -5,11 +5,20 @@
 let open_account (initial_pass: passwd) : bank_account =
   let password = ref initial_pass in 
   let balance = ref 0 in 
-  {
-    update_pass = (fun oldpass newpass -> if oldpass = !password then password := newpass);
-    deposit = (fun pass amount -> if pass = !password then balance := !balance + amount);
-    retrieve = (fun pass amount -> if pass = !password then (if balance >= balance then balance := !balance - amount));
-    show_balance = (fun pass -> if pass = !password then !balance else 0)
+  let wrong_pass_count = ref 0 in
+  { 
+    update_pass = (fun oldpass newpass -> if oldpass = !password 
+                    then password := newpass 
+                    else (wrong_pass_count := !wrong_pass_count + 1; raise wrong_pass));
+    deposit = (fun pass amount -> if !wrong_pass_count > 5 
+                then raise too_many_failures else 
+                if pass = !password then (wrong_pass_count:= 0; if amount < 0 then raise negative_amount else balance := !balance + amount) 
+                else (wrong_pass_count := !wrong_pass_count + 1; raise wrong_pass)) ;
+    retrieve = (fun pass amount -> if !wrong_pass_count > 5 
+                 then raise too_many_failures 
+                 else if pass = !password then (wrong_pass_count := 0; if amount < 0 then raise negative_amount else if !balance >= amount then balance := !balance - amount else raise not_enough_balance) else (wrong_pass_count := !wrong_pass_count + 1; raise wrong_pass)) ;
+    show_balance = (fun pass -> if !wrong_pass_count > 5 then raise too_many_failures else 
+                     if pass = !password then (!balance) else (wrong_pass_count := !wrong_pass_count + 1; raise wrong_pass))
   
   }
 ;;
@@ -26,14 +35,20 @@ let myfn_with_2_args_tests = [
 ]
 *)
 
+let g1 = {nodes = ["a"; "b"]; edges = [("a", "b", 1)]};;
+let g2 = {nodes = ["a"; "b"]; edges = []};;
+
 (* We've added a type annotation here so that the compiler can help
    you write tests of the correct form. *)
 let neighbours_tests: ((string graph * string) * (string * weight) list) list = [
+  ((g1, "a"), [("b", 1)]);
+  ((g2, "a"), [])
 ]
 
 (* TODO: Implement neighbours. *)
-let neighbours (g: 'a graph) (vertex: 'a) : ('a * weight) list =
-  notimplemented ()
+let neighbours (g: 'a graph) (vertex: 'a) : ('a * weight) list = 
+  List.map (fun (_, v2, w) -> (v2, w)) (List.filter (fun (v1, _, _) -> v1 = vertex) g.edges);;
+  
 
 (* TODO: Implement find_path. *)
 let find_path (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
